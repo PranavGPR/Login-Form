be51509e58bd4665a111329815c39a8ca2ff33b3
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-03-23 15:54:49 +0530

    functionality added

---
+++ index.js
@@ -0,0 +1,24 @@
+const username = document.querySelector("#username");
+const password = document.querySelector("#password");
+const error_message = document.querySelector(".error");
+const submit_form = document.querySelector("#login-form");
+
+submit_form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  error_message.style.color = "red";
+
+  if (!username.value && !password.value)
+    return (error_message.innerHTML =
+      "Both fields are empty. Data required to perform login!");
+
+  if (!username.value)
+    return (error_message.innerHTML = "Username can't be empty");
+
+  if (!password.value)
+    return (error_message.innerHTML = "Password can't be empty");
+
+  error_message.innerHTML = "You're logged in.";
+
+  error_message.style.color = "green";
+});
